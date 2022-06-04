094cc2b6770bbab39115e9c86748f8b33cf775bd
slaterhaus <james@yieldx.app>
2022-06-04 09:10:23 -0400

    assets path

--- libs/tricks/src/lib/tricks.module.ts
+++ libs/tricks/src/lib/tricks.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PhaseBlockComponent } from './phase-block/phase-block.component';
+import { PaintBlockComponent } from './paint-block/paint-block.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [PhaseBlockComponent],
+  declarations: [PhaseBlockComponent, PaintBlockComponent],
 })
 export class TricksModule {}
